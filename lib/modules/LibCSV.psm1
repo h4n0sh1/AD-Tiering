af615811b8fac9aba90489ba8d626aac1293387e
h4n0sh1 <h4n0sh1@gmail.com>
2025-05-09 22:54:20 +0200

    Formatting

--- lib/modules/LibCSV.psm1
+++ lib/modules/LibCSV.psm1
@@ -5,10 +5,10 @@
 # Created: 05/09/2025
 #>
 
-function Import-MapFromCsv([String]$csv_path = $TieringMap_Path,
-                         [Int]$key_index = 0,
-                         [Int]$value_index = 2
-                        ){
+function Import-MapFromCsv([String]$csv_path,
+                           [Int]$key_index = 0,
+                           [Int]$value_index = 2
+                          ){
     $map = @{}
     $csv = Import-Csv -Path $csv_path
     $key_header = $csv[0].PSObject.Properties.Name[$key_index]
